f182350ddb542166ada7724dd7a3355943bbcf8a
Hassan Habib <hassanhabib@live.com>
2020-09-23 21:23:00 -0700

    Add project files.

---
+++ ConsoleApp12/Migrations/20200924041935_StudentsAndTeachers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp12.Migrations
+{
+    public partial class StudentsAndTeachers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentTeachers",
+                columns: table => new
+                {
+                    StudentId = table.Column<Guid>(nullable: false),
+                    TeacherId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentTeachers", x => new { x.TeacherId, x.StudentId });
+                    table.ForeignKey(
+                        name: "FK_StudentTeachers_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_StudentTeachers_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentTeachers_StudentId",
+                table: "StudentTeachers",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentTeachers");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid studentId = Guid.NewGuid();
+            Guid teacherId = Guid.NewGuid();
+
+            var student = new Student
+            {
+                Id = studentId,
+                Name = "Anas",
+                StudentTeachers = new List<StudentTeacher>
+                {
+                    new StudentTeacher
+                    {
+                        StudentId = studentId,
+                        TeacherId = teacherId,
+                        Teacher = new Teacher
+                        {
+                            Id = teacherId,
+                            Name = "Hassan"
+                        }
+                    }
+                }
+            };
+
+            var dbContext = new DataContext();
+            dbContext.Students.Add(student);
+            dbContext.SaveChanges();
+
+            Student storageStudent = 
+                dbContext.Students.Find(studentId);
+
+            Console.WriteLine(storageStudent.Name);
+        }
+    }
+
+    public class DataContext: DbContext
+    {
+        public DataContext()
+        {
+            this.Database.Migrate();
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<StudentTeacher> StudentTeachers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentTeacher>().HasKey(sc => new { sc.TeacherId, sc.StudentId });
+
+            modelBuilder.Entity<StudentTeacher>()
+                .HasOne(studentTeacher => studentTeacher.Teacher)
+                .WithMany(teacher => teacher.StudentTeachers)
+                .HasForeignKey(studentTeacher => studentTeacher.TeacherId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<StudentTeacher>()
+                .HasOne(studentTeacher => studentTeacher.Student)
+                .WithMany(teacher => teacher.StudentTeachers)
+                .HasForeignKey(studentTeacher => studentTeacher.StudentId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=AnasDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+
+
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<StudentTeacher> StudentTeachers { get; set; }
+    }
+
+    public class Teacher
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<StudentTeacher> StudentTeachers { get; set; }
+    }
+
+    public class StudentTeacher {
+        public Guid StudentId { get; set; }
+        public Student Student { get; set; }
+
+        public Guid TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+    }
+}
